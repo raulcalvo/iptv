fe89b014aadbe99f0f435a44c432d54fbf67a787
Raul Calvo <raulcalvo@gmail.com>
2021-06-02 23:59:09 +0200

    Added logs to console

--- index.js
+++ index.js
@@ -90,6 +90,7 @@ function downloadPage(url) {
     return new Promise((resolve, reject) => {
         request(url, (error, response, body) => {
             if (error) reject(error);
+            console.log(JSON.stringify(response));
             if (response.statusCode != 200) {
                 reject('Invalid status code <' + response.statusCode + '>');
             }
